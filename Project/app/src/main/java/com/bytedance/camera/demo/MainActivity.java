9aa14fe380a799b75d2149e7db226b6ef9d6ab8b
tjc1411 <1599480206@qq.com>
2020-07-16 16:08:23 +0800

    hw8

---
+++ Project/app/src/main/java/com/bytedance/camera/demo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.bytedance.camera.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] permissions = new String[] {
+
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_picture).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, TakePictureActivity.class));
+        });
+
+        findViewById(R.id.btn_camera).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, RecordVideoActivity.class));
+        });
+
+    }
+
+}
---
+++ Project/app/src/main/java/com/bytedance/camera/demo/TakePictureActivity.java
@@ -0,0 +1,101 @@
+package com.bytedance.camera.demo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bytedance.camera.demo.utils.Utils;
+
+import java.io.File;
+
+public class TakePictureActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_EXTERNAL_STORAGE = 101;
+    private File imageFile;
+    String[] permissions = new String[] {
+            Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_picture);
+        imageView = findViewById(R.id.img);
+        findViewById(R.id.btn_picture).setOnClickListener(v -> {
+            if (Utils.isPermissionsReady(TakePictureActivity.this,permissions)) {
+                takePicture();
+            } else {
+                Utils.reuqestPermissions(TakePictureActivity.this,permissions,REQUEST_EXTERNAL_STORAGE);
+            }
+        });
+
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imageFile = Utils.getOutputMediaFile(Utils.MEDIA_TYPE_IMAGE);
+        if(imageFile != null) {
+            Uri fileUri = FileProvider.getUriForFile(this,
+                    "com.bytedance.camera.demo", imageFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            setPic();
+        }
+    }
+
+    private void setPic() {
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        BitmapFactory.Options bmpOptions = new BitmapFactory.Options();
+        bmpOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFile.getAbsolutePath(), bmpOptions);
+
+        int photoW = bmpOptions.outWidth;
+        int photoH = bmpOptions.outHeight;
+
+        int ScaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        bmpOptions.inJustDecodeBounds = false;
+        bmpOptions.inSampleSize = ScaleFactor;
+        bmpOptions.inPurgeable = true;
+
+        Bitmap bmp = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), bmpOptions);
+
+
+        bmp = Utils.rotateImage(bmp, imageFile.getAbsolutePath());
+
+
+        imageView.setImageBitmap(bmp);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                if (Utils.isPermissionsReady(TakePictureActivity.this,permissions)){
+                    takePicture();
+                }
+                break;
+            }
+        }
+    }
+}
